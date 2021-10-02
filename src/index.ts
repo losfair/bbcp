7e2d526a781c7d0ad7f4787231603cc1cfaa15ac
losfair <zhy20000919@hotmail.com>
2021-10-02 07:04:59 +0000

    rename

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /// <reference path="../node_modules/jsland-types/src/index.d.ts" />
 
 import "./session";
-import "./weblogic";
+import "./applogic";
 
 Router.get("/", () => new Response("rwcp ok"));
